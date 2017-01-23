548820eaabd3d03e793242c210631b16e245a287
Adrian Robotka <robotka.adrian@gmail.com>
2017-01-23 20:28:51 +0100

    add a primitive algo

--- main.cpp
+++ main.cpp
@@ -2,7 +2,45 @@
 
 using namespace std;
 
-int main()
-{
+/**
+ * Do Collatz algorithm on the number
+ *
+ * @param numberToTest Number to enumerate
+ */
+inline void enumerateNumber(unsigned long long int numberToTest) {
+    while (numberToTest != 1) {
+        if (numberToTest % 2 == 0) {
+            numberToTest = numberToTest >> 1;
+        } else {
+            numberToTest *= 3;
+            numberToTest++;
+        }
+    }
+}
+
+/**
+ * Entry point
+ *
+ * @return 0 or noting, in better case ;)
+ */
+int main() {
     ios::sync_with_stdio(false);
+
+    unsigned long long int numberToTest = 2;
+    unsigned short int index = 1;
+
+    // until overflow
+    while (numberToTest != 1) {
+        if (index == 1) {
+            cout << ".";
+            cout.flush();
+        }
+
+        enumerateNumber(numberToTest);
+
+        numberToTest++;
+        index++;
+    }
+
+    return 0;
 }
